4b1abc1870ac82521db23e013ec94b307dc3e378
Eric Jeon <bjjeon@humaxdigital.com>
2017-04-20 09:24:37 +0900

    Add json parser library . add url decoder function

--- Makefile
+++ Makefile
@@ -1,22 +1,21 @@
 APP_DIR=./apps
-webserver: $(APP_DIR)/webserver.c libuv/.libs/libuv.a libhttp_parser/http_parser.o
+webserver: $(APP_DIR)/webserver.c libuv/.libs/libuv.a libhttp_parser/http_parser.o libjason_parser/jsmn.o
 #	gcc -DEV_MULTIPLICITY=0  -o webserver $(APP_DIR)/webserver.c libuv/.libs/libuv.a -lpthread
 #	gcc -o webserver $(APP_DIR)/webserver.c libuv/.libs/libuv.a libhttp_parser/libhttp_parser.a  -lpthread
-	gcc -o webserver $(APP_DIR)/webserver.c libuv/.libs/libuv.a libhttp_parser/http_parser.o  -lpthread  
+	gcc -o webserver $(APP_DIR)/webserver.c libuv/.libs/libuv.a libhttp_parser/http_parser.o libjason_parser/jsmn.o -lpthread  
 
 libuv/.libs/libuv.a: 
 	$(MAKE) -C libuv 
 
-#libhttp_parser/libhttp_paraser.a 
-#	$(MAKE) -C libhttp_parser pacakge
-#
-libhttp_parser/http_parser.o:
-	$(MAKE) -C http_parser.o 
+libhttp_parser/libhttp_paraser.a:
+	$(MAKE) -C libhttp_parser pacakge
 
-
+libjason_parser/jsmn.o:
+	$(MAKE) -C libjason_parser jsmn.o
 
 clean:
 	$(MAKE) -C libuv clean
 	$(MAKE) -C libhttp_parser clean
+	$(MAKE) -C libjason_parser clean
 	rm webserver
 	
--- apps/webserver.c
+++ apps/webserver.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include "../libuv/include/uv.h"
 #include "../libhttp_parser/http_parser.h"
+#include "../libjason_parser/jsmn.h"
 
 
 /* customizing value */
 #define SERVER_IP "0.0.0.0"
-#define SERVER_PORT 8000
-
+#define SERVER_PORT 1337
+#define NUM_JASON_FIELD 256
 #define DEFAULT_BACKLOG 10   /* indicates the number of connections the kernel might queue */
 
 #define BLOCK_HTTP_PARSE_MESSAGE
@@ -26,7 +27,12 @@ static server_t listenServer;
 typedef struct {
     uv_tcp_t tcpHandle;         /* client socket */
     server_t *pServerHandle;    /* listening socket + event loop */
+
+    /* parsers */
     http_parser stHttpParser; 
+    jsmn_parser stJsonParser;
+    jsmntok_t *pJsonToken; 
+
     uv_write_t  *write_req;
 
     /* REST related */
@@ -109,20 +115,195 @@ int onHeaders_http_cb(http_parser* pHttpParser)
 //  fprintf(stderr, " httpMethod : %s\n", http_method_str(pClient->eHttpMethod));
     return 0;
 }
+int SE_Utils_Htoi(char *c_hexa)
+{
+	int deci = 0;
+	const char *sp = c_hexa;
+	while(*sp) {
+		deci *= 16;
+		char ch = 0;
+		if('0' <= *sp && *sp <= '9')
+			ch = *sp - '0';
+		if('A' <= *sp && *sp <= 'F')
+			ch = *sp - 'A' + 10;
+		if('a' <= *sp && *sp <= 'f')
+			ch = *sp - 'a' + 10;
+		deci += ch;
+		sp++;
+	}
+	return deci;
+}
+
+void SE_PH_HTTPAsciiToHexConvert(char *dest, char *src, unsigned int size)
+{   
+	unsigned int src_idx = 0;
+	unsigned int dest_idx = 0;
+	char hex_string[3];
+	for(src_idx = 0; src_idx< size;src_idx++){
+		if(src[src_idx] == 37){ /* find "%" string of JSON format from HTTP stream */
+			src_idx++;
+			memset(hex_string,0x00,5);
+			memcpy(hex_string,&src[src_idx],2);
+			src_idx += 1;
+			dest[dest_idx] = SE_Utils_Htoi((char *)hex_string);
+		}
+		else{
+			dest[dest_idx] = src[src_idx];
+		}
+		dest_idx++;
+	}
+}
+
+
+
+
+/*
+ * An example of reading JSON from stdin and printing its content to stdout.
+ * The output looks like YAML, but I'm not sure if it's really compatible.
+ */
+
+static int dump(const char *js, jsmntok_t *t, size_t count, int indent) {
+	int i, j, k;
+	if (count == 0) {
+                printf(" count is zero, return\n");
+		return 0;
+	}
+	if (t->type == JSMN_PRIMITIVE) {
+		printf("%.*s", t->end - t->start, js+t->start);
+		return 1;
+	} else if (t->type == JSMN_STRING) {
+		printf("'%.*s'", t->end - t->start, js+t->start);
+		return 1;
+	} else if (t->type == JSMN_OBJECT) {
+		printf("\n");
+		j = 0;
+		for (i = 0; i < t->size; i++) {
+			for (k = 0; k < indent; k++) printf("  ");
+			j += dump(js, t+1+j, count-j, indent+1);
+			printf(": ");
+			j += dump(js, t+1+j, count-j, indent+1);
+			printf("\n");
+		}
+		return j+1;
+	} else if (t->type == JSMN_ARRAY) {
+		j = 0;
+		printf("\n");
+		for (i = 0; i < t->size; i++) {
+			for (k = 0; k < indent-1; k++) printf("  ");
+			printf("   - ");
+			j += dump(js, t+1+j, count-j, indent+1);
+			printf("\n");
+		}
+		return j+1;
+	}
+	return 0;
+}
+static const char *JSON_STRING =
+	"{\"cmd\":\"front/output/led\", \"led_no\":\"0\"}";
+
+
+
+void urlDecoder(char * dst , const char * src)
+{
+    char a,b;
+    while (*src)
+    {
+        if ((*src == '%') && 
+            ((a = src[1]) && (b = src[2])) &&
+            isxdigit(a) && isxdigit(b) )  
+        {
+            if (a >= 'a')
+                a -= 'a'-'A';
+            if (a >= 'A')
+                a -= ('A' - 10);
+            else
+                a -= '0';
+            if (b >= 'a')
+                b -= 'a'-'A';
+            if (b >= 'A')
+                b -= ('A' - 10);
+            else
+                b -= '0';
+            *dst++ = 16*a+b;
+            src+=3;
+        } 
+        else if (*src == '+') 
+        {
+            *dst++ = ' ';
+            src++;
+        } 
+        else 
+        {
+            *dst++ = *src++;
+        }
+    }
+    *dst++ = '\0';
+    return;
+}
+
 
 int onBody_http_cb(http_parser* pHttpParser, const char *at, size_t length)
 {
     client_t *pClient = NULL;
-
+    int returnCode = 0;
     pClient = (client_t *) pHttpParser->data;
-
+    char * data = NULL;
     //printf(" httpMethod : %d\n", (pClient->eHttpMethod));
     /* PARSE JASON */
     if ( pClient->eHttpMethod == 3) /* POST */
     {
+
         printf("Receive POST so Start to parse  JASON \n");
-        printf(" for example, just dump boyd \n");
+#if 1
+        printf("dump body \n");
         printf("%s\n", at);
+#endif
+        jsmn_init(&pClient->stJsonParser);
+        pClient->pJsonToken = malloc(sizeof(jsmntok_t) * NUM_JASON_FIELD);
+#if 0
+        at = at + 8;
+        length = length - 8;
+
+        printf(" Length from server %zd - 8 \n", length);
+        data = malloc( length+1 );
+        bzero (data, length+1  );
+        memcpy(data, at , length );
+        
+        SE_PH_HTTPAsciiToHexConvert(data, at, length);
+        printf("converted string : %s \n", data); 
+        printf (" sizeof %zd strlen %zd \n", length, strlen(data));
+#endif
+        at = at + 8;
+        data = malloc(length);
+        bzero (data, length);
+ 
+        /* need to %xx to ASCII */
+        urlDecoder(data , at );
+        printf("converted string : %s \n", data);
+
+        returnCode = jsmn_parse(&pClient->stJsonParser, data, length, pClient->pJsonToken, NUM_JASON_FIELD);      
+#if 0 
+        returnCode = jsmn_parse(&pClient->stJsonParser, at, length, pClient->pJsonToken, NUM_JASON_FIELD);      
+        printf (" sizeof %ld strlen %zd \n", sizeof(JSON_STRING), strlen(JSON_STRING));
+        returnCode = jsmn_parse(&pClient->stJsonParser, JSON_STRING, strlen(JSON_STRING), pClient->pJsonToken, NUM_JASON_FIELD);      
+#endif
+        if (returnCode  < 0) 
+        {
+            if (returnCode  == JSMN_ERROR_NOMEM) 
+            {
+                fprintf(stderr, " Need more jason token");
+            } 
+            else
+            {
+                fprintf(stderr, " Json parsing error : %d ", returnCode);
+            }
+        }
+        else
+        {
+            printf(" dump Jason - count : %d \n", pClient->stJsonParser.toknext);
+            //dump(js, tok, p.toknext, 0);
+            dump(data, pClient->pJsonToken, pClient->stJsonParser.toknext, 0); 
+        }
     }
     return 0;
 }
